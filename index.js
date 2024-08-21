71cddbe7f7ab3bfdf04e5a516fb7f31f6f1d5c1d
Andrew Bootsma <andrew_boots@proton.me>
2024-08-20 20:04:37 -0400

    small tweaks

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const http = require('http');
+
+const server = http.createServer((_, res) => {
+  res.writeHead(404, {
+    'Content-type': 'text/html',
+  });
+  res.end('<h1>Page not found!</h1>');
+});
+
+server.listen(8000, '127.0.0.1', () => {});
