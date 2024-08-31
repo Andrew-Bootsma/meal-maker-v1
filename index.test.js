7869d8e1c9a600b8ed7d40948d4654099a819d45
Andrew Bootsma <andrew_boots@proton.me>
2024-08-31 16:07:38 -0400

    test for images

--- index.test.js
+++ index.test.js
@@ -30,6 +30,17 @@ describe('GET /', () => {
   });
 });
 
+describe('GET /images/liver-and-onions.jpg', () => {
+  it('should return a 200 status and the correct content-type for the image', async () => {
+    const response = await request(server).get('/images/liver-and-onions.jpg');
+
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toBe('image/jpeg');
+    expect(response.body).toBeInstanceOf(Buffer); // Ensure the response is a buffer (binary data)
+    expect(response.body.length).toBeGreaterThan(0); // Ensure the image has content
+  });
+});
+
 describe('GET /non-existent-path', () => {
   it('should return a 404 status and a "Page not found!" message', async () => {
     const response = await request(server).get('/non-existent-path');
