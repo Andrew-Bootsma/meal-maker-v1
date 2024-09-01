59ef9c1881550dfdcb53b79579258a622b3243df
Andrew Bootsma <andrew_boots@proton.me>
2024-09-01 13:11:44 -0400

    images and recipe styles

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -2,6 +2,12 @@ module.exports = (temp, recipe) => {
   let output = temp.replace(/{%RECIPENAME%}/g, recipe.recipeName);
   output = output.replace(/{%ID%}/g, recipe.id);
   output = output.replace(/{%IMAGE%}/g, recipe.image);
+  output = output.replace(/{%INGREDIENGS%}/g, recipe.ingredients);
+  output = output.replace(/{%DESCRIPTION%}/g, recipe.description);
+  output = output.replace(/{%DIRECTIONS%}/g, recipe.directions);
+  output = output.replace(/{%SERVINGSIZE%}/g, recipe.servingSize);
+  output = output.replace(/{%SERVINGTYPE%}/g, recipe.servingType);
+  output = output.replace(/{%PROTEIN%}/g, recipe.protein);
 
   if (!recipe.highProtein)
     output = output.replace(/{%NOT_HIGHPROTEIN%}/g, 'notHighProtein');
